42961ab86fa0accf3db8e5782b90ad0a23373c77
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2024-02-12 13:09:06 -0700

    Misc updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "";
+export const SITE_TITLE = "Austin Abbott's blog";
 export const SITE_DESCRIPTION =
   "A place for me to host some dev notes, solutions I've found, and miscellaneous thoughts";
--- src/content/blog/first-post.md
+++ src/content/blog/first-post.md
@@ -5,4 +5,6 @@ pubDate: "Feb 12 2024"
 heroImage: "/blog-placeholder-3.jpg"
 ---
 
-First post on the new blog, woo!
+First post on the new blog, woo-hoo!
+
+Stay tuned, I'll have more interesting content down the road.
