2407c457acbf615c29be441878853a45959015e0
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2024-02-12 09:45:01 -0700

    MVP

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "";
+export const SITE_DESCRIPTION =
+  "A place for me to host some dev notes, solutions I've found, and miscellaneous thoughts";
